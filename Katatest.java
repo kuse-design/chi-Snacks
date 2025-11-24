04b9e8d7c37ab8bc1acde433573e760d3a5170c5
Kuse_design <0luwakusejerry1@gmail.com>
2025-11-24 08:31:05 +0100

    Kata

---
+++ Katatest.java
@@ -0,0 +1,11 @@
+public class Katatest{
+public static void main(String[] args){
+
+System.out.println(Katafunction.even(10));
+System.out.println(Katafunction.primeNumber(10));
+System.out.println(Katafunction.square(10));
+System.out.println(Katafunction.squareBoolean(10));
+System.out.println(Katafunction.subtract(10, 2));
+
+}
+}
